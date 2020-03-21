8565ae85e6999b71df3cc50a62644ad8c3ca8598
unknown <jadams@peoplenetonline.com>
2020-03-21 09:45:45 -0500

    adding PyBank

---
+++ PyBank/main.py
@@ -0,0 +1,71 @@
+# First we'll import the os module
+# This will allow us to create file paths across operating systems
+import os
+
+# Module for reading CSV files
+import csv
+
+# declare csv file to be read
+#csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
+csvpath = os.path.join('Resources', 'budget_data.csv')
+
+# initiate variables
+months = 0
+profit = 0
+# set to artificially low/high values, so first row will
+# automatically get seeded as max/min.
+maxprofitdollar = -999999999999
+minprofitdollar = 999999999999
+
+# open csv file
+with open(csvpath, newline='') as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    # Read the header row first (skip this step if there is now header)
+    csv_header = next(csvreader)
+    # print(f"CSV Header: {csv_header}")
+
+    # Now, read each row of data after the first row
+    for row in csvreader:
+            # tally of number of months
+            months += 1
+
+            # running total of profit/loss
+            profit += int(row[1])
+            
+            # compare current row to maxprofitdollar 
+            # if larger, this is new max month
+            if int(row[1]) > int(maxprofitdollar):
+                maxprofitmonth = row[0]
+                maxprofitdollar = row[1]
+
+            # compare current row to minprofitdollar
+            # if less, this is the new min month
+            if int(row[1]) < int(minprofitdollar):
+                minprofitmonth = row[0]
+                minprofitdollar = row[1]
+
+    # calculate average monthly profit
+    averageprofit = profit / months
+
+    # Display output to screen
+    print ("Financial Analysis")
+    print ("------------------")
+    print ("Total Months: " + str(months))
+    print ("Total: ${0:<12,.0f}".format(profit))
+    print ("Average Change: ${0:<12,.2f}".format(averageprofit))
+    print ("Greatest Increase in Profits: "  + maxprofitmonth + " ($" + maxprofitdollar + ")")
+    print ("Greatest Decrease in Profits: "  + minprofitmonth + " ($" + minprofitdollar + ")")
+
+# Push the output to a text file
+with open("financial_analysis.txt", "w") as f:
+    print ("Financial Analysis", file=f)
+    print ("------------------", file=f)
+    print ("Total Months: " + str(months), file=f)
+    print ("Total: ${0:<12,.0f}".format(profit), file=f)
+    print ("Average Change: ${0:<12,.2f}".format(averageprofit), file=f)
+    print ("Greatest Increase in Profits: "  + maxprofitmonth + " ($" + maxprofitdollar + ")", file=f)
+    print ("Greatest Decrease in Profits: "  + minprofitmonth + " ($" + minprofitdollar + ")", file=f)
+    
